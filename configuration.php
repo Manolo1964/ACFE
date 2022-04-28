1c417a2191d901ca8d71cdf0d599daeac0401c47
Manolo <lolosub@gmail.com>
2022-04-28 21:01:47 +0200

    configuration + registre

---
+++ configuration.php
@@ -0,0 +1,17 @@
+﻿﻿<?php 
+
+//Données du serveur et de la base de données
+
+$server="sql304.260mb.net";
+$username="n260m_31608269";
+$password= "xxxxxx"; 
+$database_name="xxxxxxx";
+//La connexion avec le serveur est établie
+$conexion=mysql_connect($server, $username, $password) 
+or die("Difficultés rencontrées pour établir la connexion");
+
+//Seleccionamos la base de datos 
+$bd_sel=mysql_select_db($database_name) or die("Problèmes de sélection de la base de données");
+?>
+
+
---
+++ registre.php
@@ -0,0 +1,57 @@
+﻿﻿<!DOCTYPE HTML>
+<html>
+<html lang="fr">
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<head>
+
+</head>
+
+<body>
+<p id"O">   
+
+<?php
+
+$ID=$_POST['ID'];
+$message=$_POST['message'];  
+$nom=$_POST['nom']; 
+$email=$_POST['email']; 
+$telephone=$_POST['telephone'];
+
+
+include("configuration.php");
+
+
+
+//je vérifie si l’utilisateur est déjà enregistré 
+$query="select * from registre_utilisateurs where idUser=".$ID;
+
+$result=mysql_query($query) or die("Erreur dans l’instruction SQL");
+
+if (mysql_num_rows($result) > 0) {
+	echo"<br/><br/>";
+	echo "L’Utilisateur est déjà enregistré </br>";
+	echo "<a href=../index.php>Retourner</a>";
+
+} else if (mysql_num_rows($result) == 0) {
+//nous enregistrons l’utilisateur dans l’association
+  
+$query="insert into registre_utilisateurs(idUser,email,phone,nom,message)
+	values($ID,'$email','$phone','$nom','$message')";
+	$result=mysql_query($query) or die("Erreur d’exécution de l’instruction SQL ".mysql_error());
+	
+	echo"<br/><br/>";
+	echo "<br/>";
+       	echo "<a href=../index.html>Retourner</a>";
+	mysql_close();          
+        
+
+}
+
+?>
+ <!-- javascript -->
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.js"></script>
+
+  </body>
+</html>
